89a48aa9eba3408094fc0dfc96a117f2c3cc55e9
Bailey Stoner <monokrome@monokro.me>
2018-05-17 08:26:56 -0700

    Add caching and simplify URLs.

--- mtc/shorten/managers.py
+++ mtc/shorten/managers.py
@@ -3,9 +3,14 @@ from django.db.models.query import QuerySet
 
 from mtc.shorten import util
 
+
+def padded(identifier: str) -> bytes:
+    return identifier + '=='
+
+
 class ShortenedLocationManager(Manager):
     def by_short_identifier(self, identifier: str) -> QuerySet:
-        return self.filter(util.decode(identifier))
+        return self.filter(util.decode(padded(identifier)))
 
     def get_by_short_identifier(self, identifier: str) -> 'ShortenedLocation':
-        return self.get(pk=util.decode(identifier))
+        return self.get(pk=util.decode(padded(identifier)))
--- mtc/shorten/views.py
+++ mtc/shorten/views.py
@@ -3,11 +3,17 @@ import json
 from django import http
 from django import views
 
+from django.views.decorators.cache import cache_page
+
 from mtc.shorten import models
 
 def location_from_identifier(identifier: str):
-    return models.ShortenedLocation.objects.get_by_short_identifier(identifier)
+    try:
+        return models.ShortenedLocation.objects.get_by_short_identifier(identifier)
+    except ValueError as e:
+        raise http.Http404(str(e))
 
+@cache_page(60)
 def preview(request: http.HttpRequest, *, identifier: bytes) -> http.HttpResponse:
     """ TODO: Make pretty someday? """
 
@@ -15,7 +21,7 @@ def preview(request: http.HttpRequest, *, identifier: bytes) -> http.HttpRespons
         'location': location_from_identifier(identifier).original_url,
     }))
 
-
+@cache_page(3600)
 def redirect(request: http.HttpRequest, *, identifier: bytes) -> http.HttpResponse:
     return http.HttpResponseRedirect(
         redirect_to=location_from_identifier(identifier).original_url,
