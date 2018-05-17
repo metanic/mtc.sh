8bc24f6508ee1adbba41495a4ccc1bc82ec4ad8c
Bailey Stoner <monokrome@monokro.me>
2018-05-17 08:59:52 -0700

    Allow mtc.sh

--- mtc/settings/production.py
+++ mtc/settings/production.py
@@ -2,16 +2,17 @@ import dj_database_url
 
 from mtc.settings.defaults import *  # noqa
 
+ALLOWED_HOSTS = [
+    'mtc.sh',
+]
 
 CACHES = {
     'default': cache_url(env_value('redis_url')),
 }
 
-
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500),
 }
-
 
 RAVEN_CONFIG = {
     'dsn': env_value('sentry_dsn'),
