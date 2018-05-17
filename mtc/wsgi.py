daa3c7378068805162df7fad445bdfac81502634
Bailey Stoner <monokrome@monokro.me>
2018-05-17 02:21:52 -0700

    Ensure production configuration w/ UWSGI

--- mtc/wsgi.py
+++ mtc/wsgi.py
@@ -6,6 +6,6 @@ from django.core import wsgi
 
 import mtc
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtc.settings.production')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mtc.settings.production'
 
 application = wsgi.get_wsgi_application()
