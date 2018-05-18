c3c53dec8fdffee3ca0eb9efdf26a58fd6ab7e92
Bailey Stoner <monokrome@monokro.me>
2018-05-17 23:15:59 -0700

    Fix padding characters to not show up in URLs.

--- mtc/shorten/util.py
+++ mtc/shorten/util.py
@@ -2,8 +2,9 @@ import base64
 import struct
 
 def decode(identifier: str) -> int:
-    return base64.b64decode(identifier)
+    pad_size = len(identifier) % 3
+    return base64.b64decode(identifier + '=' * pad_size)
 
 
 def encode(identifier: int) -> str:
-    return base64.b64encode(identifier)
+    return base64.b64encode(identifier.rstrip('='))
