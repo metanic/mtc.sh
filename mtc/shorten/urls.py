a012cf212db103c3b0b93569b6759156370e450d
Bailey Stoner <monokrome@monokro.me>
2018-05-17 23:29:30 -0700

    Preview as a character that doesn't appear in base64 encoding
    Because I'm paranoid someone will exploit or run into that, even though
    it'd just end up on a preview page

--- mtc/shorten/urls.py
+++ mtc/shorten/urls.py
@@ -3,6 +3,6 @@ from django import urls
 from mtc.shorten import views
 
 urlpatterns = (
-  urls.path('preview/<str:identifier>', views.preview),
+  urls.path('_/<str:identifier>', views.preview),
   urls.path('<str:identifier>', views.redirect),
 )
