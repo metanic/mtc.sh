01002ebd2f4aec6b19039bb26f4f9b2fb663fc58
Bailey Stoner <monokrome@monokro.me>
2018-06-22 00:15:05 -0700

    Use text field.
    This leaves me happy storing identifier, but still able to encrypt w/
    user passwords if necessary.

--- mtc/shorten/util.py
+++ mtc/shorten/util.py
@@ -8,10 +8,24 @@ from django.utils import encoding
 from django.conf import settings
 
 
-def decode(identifier: str) -> int:
+def decode(identifier: str, password: str = None) -> int:
     pad_size = len(identifier) % 3
-    return base64.b64decode(identifier + '=' * pad_size)
+    b64_content = identifier + '=' * pad_size
+
+    derived = base64.b64decode(b64_content)
+
+    if password is None:
+        return derived
+
+    # TODO: Decrypt w/ password
+    return derived
 
 
-def encode(identifier: int) -> str:
-    return base64.b64encode(identifier).rstrip('=')
+def encode(identifier: int, password: str = None) -> str:
+    stored = base64.b64encode(identifier).rstrip('=')
+
+    if password is None:
+        return stored
+
+    # TODO: Encrypt w/ password
+    return stored
